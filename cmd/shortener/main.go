9d7bad38a44b0287bc4bcf2bab563c5390e28f38
Alex Kuz <alexkuz@bk.ru>
2022-08-17 17:34:15 +0300

    tttt

---
+++ cmd/shortener/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const addr = "localhost:8080"
+
+func main() {
+
+	http.HandleFunc("/", Handler)
+	http.ListenAndServe(addr, nil)
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case "GET":
+
+		loc := "http://localhost:8080/abc"
+
+		//w.Header().Set("Location", loc) //http://localhost:8080"http://localhost:8080/"
+		//w.WriteHeader(http.StatusTemporaryRedirect)
+		//w.Write([]byte(res))
+
+		http.Redirect(w, r, loc, http.StatusTemporaryRedirect)
+
+	default:
+
+		fmt.Fprintf(w, r.Method+" - Метод не поддерживается")
+
+	}
+
+}
